ec6f561ae3db19e5a6b697d1ee241f7d50b27a54
yektaonurylmz <40947943+yektaonurylmz@users.noreply.github.com>
2020-12-25 00:22:07 +0300

    Update

--- src/com/company/BuyukUnluUyumu.java
+++ src/com/company/BuyukUnluUyumu.java
@@ -1,43 +1,31 @@
 package com.company;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class BuyukUnluUyumu {
 
     public static void main(String[] args) {
-        System.out.println("Kelime Giriniz : ");
 
+        System.out.println("Kelime Giriniz : ");
         Scanner scn = new Scanner(System.in);
         String kelime = scn.next();
 
         buyukunlu(kelime);
     }
         public static void buyukunlu (String kelime) {
-            int sayac1 = 0;
-            int sayac2 = 0;
+
             String[] harf_ayir =kelime.split("");
-            ArrayList<String> list = new ArrayList<String>(Arrays.asList(harf_ayir));
-            String[] k_harfler = {"A", "a", "I", "ı", "O", "o", "U", "u"};       // Kalın Harfler
-            String[] i_harfler = {"E", "e", "İ", "i", "Ö", "ö", "Ü", "ü"};       // İnce  Harfler
+            ArrayList<String> list = new ArrayList<>(Arrays.asList(harf_ayir));
+            String[] k_harfler = {"A", "a", "I", "ı", "O", "o", "U", "u"};
+            String[] i_harfler = {"E", "e", "İ", "i", "Ö", "ö", "Ü", "ü"};
+            boolean kalinHarfKontrol = Arrays.stream(k_harfler).anyMatch(list::contains);
+            boolean inceHarfKontrol = Arrays.stream(i_harfler).anyMatch(list::contains);
+            if (kalinHarfKontrol && inceHarfKontrol){
+                System.out.println("Buyuk Ünlü Uyumuna Uymaz");
 
-            for (int i=0; i<k_harfler.length;i++){
-                if (list.contains(k_harfler[i])){
-                    sayac1++;
-                }
-                if (list.contains(i_harfler[i])){
-                    sayac2++;
-                }
-            }
-
-            if (sayac1 > 0 && sayac2 > 0) {
-                System.out.println("Büyük Ünlü Uyumuna uymaz.");
-            } else if (sayac1 > 0) System.out.println("Büyük Ünlü Uyumuna uyar.");
-            else if (sayac2 > 0) System.out.println("Büyük Ünlü Uyumuna uyar.");
-
+            }else System.out.println("Buyuk Ünlü Uyumuna Uyar");
         }
 
 
